cc817fd0544f778ace46dd0736aa2e3d8ff1958d
Michael Maurer <mjmaurer777@gmail.com>
2019-05-16 23:16:18 -0400

    setup

---
+++ setup.sh
@@ -0,0 +1,9 @@
+ln -s -f ~/dot/.vimrc ~/.vimrc
+ln -s -f ~/dot/.bashrc ~/.bashrc
+ln -s -f ~/dot/.bash_logout ~/.bash_logout
+ln -s -f ~/dot/.bash_profile ~/.bash_profile
+ln -s -f ~/dot/.tmux.conf ~/.tmux.conf
+ln -s -f ~/dot/.profile ~/.profile
+ln -s -f ~/dot/intellij/.ideavimrc ~/.ideavimrc
+ln -s -f ~/dot/win/.bash_history ~/.bash_history
+
